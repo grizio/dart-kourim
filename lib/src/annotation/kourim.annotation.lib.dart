321e69fc0ee108689e6b91d4d24a8a5e5bb56e7a
Gaëtan Rizio <gaetan@rizio.fr>
2015-03-29 23:22:42 +0200

    Deletion after restructuration

--- lib/src/annotation/kourim.annotation.lib.dart
+++
@@ -1,6 +0,0 @@
-/// This library defines all annotations used by Kourim system.
-library kourim.annotation;
-
-import 'package:kourim/constants.dart' as constants;
-
-part 'model.dart';
--- lib/src/core/interface/kourim.core.interface.dart
+++
@@ -1,9 +0,0 @@
-library kourim.core.interface;
-
-import 'package:klang/klang.dart';
-import 'dart:mirrors';
-
-part 'IConverter.dart';
-part 'IMapper.dart';
-part 'IModelDescription.dart';
-part 'IModelValidation.dart';
